308ace09e14dd4b25115c604d17cdc1764080e9f
VISTALL <vistall.valeriy@gmail.com>
2020-06-03 11:37:57 +0300

    compilation

--- src/main/java/consulo/bash/module/extension/BashMutableModuleExtension.java
+++ src/main/java/consulo/bash/module/extension/BashMutableModuleExtension.java
@@ -1,20 +1,25 @@
 package consulo.bash.module.extension;
 
-import com.ansorgit.plugins.bash.settings.facet.ui.BashFacetUI;
-import com.intellij.openapi.ui.VerticalFlowLayout;
-import consulo.module.extension.MutableModuleExtension;
-import consulo.roots.ModuleRootLayer;
-import consulo.ui.annotation.RequiredUIAccess;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.swing.*;
+
+import com.ansorgit.plugins.bash.settings.facet.ui.BashFacetUI;
+import com.intellij.openapi.ui.VerticalFlowLayout;
+import consulo.disposer.Disposable;
+import consulo.module.extension.MutableModuleExtension;
+import consulo.module.extension.swing.SwingMutableModuleExtension;
+import consulo.roots.ModuleRootLayer;
+import consulo.ui.Component;
+import consulo.ui.Label;
+import consulo.ui.annotation.RequiredUIAccess;
+import consulo.ui.layout.VerticalLayout;
 
 /**
  * @author VISTALL
  * @since 19.11.13.
  */
-public class BashMutableModuleExtension extends BashModuleExtension implements MutableModuleExtension<BashModuleExtension>
+public class BashMutableModuleExtension extends BashModuleExtension implements MutableModuleExtension<BashModuleExtension>, SwingMutableModuleExtension
 {
 	public BashMutableModuleExtension(@Nonnull String id, @Nonnull ModuleRootLayer moduleRootLayer)
 	{
@@ -24,11 +29,9 @@ public class BashMutableModuleExtension extends BashModuleExtension implements M
 	@RequiredUIAccess
 	@Nullable
 	@Override
-	public JComponent createConfigurablePanel(@Nullable Runnable runnable)
+	public Component createConfigurationComponent(@Nonnull Disposable disposable, @Nonnull Runnable runnable)
 	{
-		JPanel panel = new JPanel(new VerticalFlowLayout(true, false));
-		panel.add(new BashFacetUI(this));
-		return panel;
+		return VerticalLayout.create().add(Label.create("Unsupported UI"));
 	}
 
 	@Override
@@ -43,5 +46,15 @@ public class BashMutableModuleExtension extends BashModuleExtension implements M
 		return myIsEnabled != extension.isEnabled() ||
 				myOperationMode != extension.getOperationMode() ||
 				!myMapping.equals(extension.myMapping);
+	}
+
+	@RequiredUIAccess
+	@Nullable
+	@Override
+	public JComponent createConfigurablePanel(@Nonnull Disposable disposable, @Nonnull Runnable runnable)
+	{
+		JPanel panel = new JPanel(new VerticalFlowLayout(true, false));
+		panel.add(new BashFacetUI(this));
+		return panel;
 	}
 }
