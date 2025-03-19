bfd2b8d1eeae3ad2bab9bda6bd00c31c25a74fd5
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-03-19 18:53:44 +0400

    migrate to editor colors schemes new api

--- src/main/java/consulo/bash/BashDarkAdditionalTextAttributesProvider.java
+++ src/main/java/consulo/bash/BashDarkAdditionalTextAttributesProvider.java
@@ -1,8 +1,12 @@
 package consulo.bash;
 
+import com.ansorgit.plugins.bash.editor.highlighting.BashSyntaxHighlighter;
 import consulo.annotation.component.ExtensionImpl;
-import consulo.colorScheme.AdditionalTextAttributesProvider;
+import consulo.colorScheme.AttributesFlyweightBuilder;
+import consulo.colorScheme.EditorColorSchemeExtender;
 import consulo.colorScheme.EditorColorsScheme;
+import consulo.colorScheme.EffectType;
+import consulo.ui.color.RGBColor;
 import jakarta.annotation.Nonnull;
 
 /**
@@ -10,16 +14,54 @@ import jakarta.annotation.Nonnull;
  * @since 11.05.2024
  */
 @ExtensionImpl
-public class BashDarkAdditionalTextAttributesProvider implements AdditionalTextAttributesProvider {
-    @Nonnull
+public class BashDarkAdditionalTextAttributesProvider implements EditorColorSchemeExtender {
     @Override
-    public String getColorSchemeName() {
-        return EditorColorsScheme.DEFAULT_SCHEME_NAME;
+    public void extend(Builder builder) {
+        builder.add(BashSyntaxHighlighter.FUNCTION_CALL, AttributesFlyweightBuilder.create()
+            .withItalicFont()
+            .build());
+
+        builder.add(BashSyntaxHighlighter.FUNCTION_DEF_NAME, AttributesFlyweightBuilder.create()
+            .withBoldFont()
+            .build());
+
+        builder.add(BashSyntaxHighlighter.HERE_DOC, AttributesFlyweightBuilder.create()
+            .withBackground(new RGBColor(0x5A, 0x5A, 0x5A))
+            .withItalicFont()
+            .build());
+
+        builder.add(BashSyntaxHighlighter.HERE_DOC_START, AttributesFlyweightBuilder.create()
+            .withBoldFont()
+            .withItalicFont()
+            .build());
+
+        builder.add(BashSyntaxHighlighter.HERE_DOC_END, AttributesFlyweightBuilder.create()
+            .withBoldFont()
+            .withItalicFont()
+            .build());
+
+        builder.add(BashSyntaxHighlighter.EXTERNAL_COMMAND, AttributesFlyweightBuilder.create()
+            .withForeground(new RGBColor(0xC5, 0x76, 0x33))
+            .withItalicFont()
+            .build());
+
+        builder.add(BashSyntaxHighlighter.INTERNAL_COMMAND, AttributesFlyweightBuilder.create()
+            .withForeground(new RGBColor(0xB0, 0xC9, 0x05))
+            .build());
+
+        builder.add(BashSyntaxHighlighter.REDIRECTION, AttributesFlyweightBuilder.create()
+            .withItalicFont()
+            .withEffect(EffectType.WAVE_UNDERSCORE, null)
+            .build());
+
+        builder.add(BashSyntaxHighlighter.SHEBANG_COMMENT, AttributesFlyweightBuilder.create()
+            .withBoldFont()
+            .build());
     }
 
     @Nonnull
     @Override
-    public String getColorSchemeFile() {
-        return "/colors/BashDarcula.xml";
+    public String getColorSchemeId() {
+        return EditorColorsScheme.DARCULA_SCHEME_NAME;
     }
 }
--- src/main/java/consulo/bash/BashLightAdditionalTextAttributesProvider.java
+++ src/main/java/consulo/bash/BashLightAdditionalTextAttributesProvider.java
@@ -1,8 +1,12 @@
 package consulo.bash;
 
+import com.ansorgit.plugins.bash.editor.highlighting.BashSyntaxHighlighter;
 import consulo.annotation.component.ExtensionImpl;
-import consulo.colorScheme.AdditionalTextAttributesProvider;
+import consulo.colorScheme.AttributesFlyweightBuilder;
+import consulo.colorScheme.EditorColorSchemeExtender;
 import consulo.colorScheme.EditorColorsScheme;
+import consulo.colorScheme.EffectType;
+import consulo.ui.color.RGBColor;
 import jakarta.annotation.Nonnull;
 
 /**
@@ -10,16 +14,41 @@ import jakarta.annotation.Nonnull;
  * @since 11.05.2024
  */
 @ExtensionImpl
-public class BashLightAdditionalTextAttributesProvider implements AdditionalTextAttributesProvider {
-    @Nonnull
+public class BashLightAdditionalTextAttributesProvider implements EditorColorSchemeExtender {
     @Override
-    public String getColorSchemeName() {
-        return EditorColorsScheme.DEFAULT_SCHEME_NAME;
+    public void extend(Builder builder) {
+        builder.add(BashSyntaxHighlighter.FUNCTION_CALL, AttributesFlyweightBuilder.create()
+            .withForeground(new RGBColor(0xC5, 0x76, 0x33))
+            .withItalicFont()
+            .build());
+
+        builder.add(BashSyntaxHighlighter.FUNCTION_DEF_NAME, AttributesFlyweightBuilder.create()
+            .withForeground(new RGBColor(0xC5, 0x76, 0x33))
+            .withBoldFont()
+            .build());
+
+        builder.add(BashSyntaxHighlighter.HERE_DOC, AttributesFlyweightBuilder.create()
+            .withBackground(new RGBColor(0xE7, 0xFF, 0xB3))
+            .build());
+
+        builder.add(BashSyntaxHighlighter.INTERNAL_COMMAND, AttributesFlyweightBuilder.create()
+            .withForeground(new RGBColor(0xB0, 0xC9, 0x05))
+            .build());
+
+        builder.add(BashSyntaxHighlighter.REDIRECTION, AttributesFlyweightBuilder.create()
+            .withForeground(new RGBColor(0x5A, 0x5A, 0x5A))
+            .withBoldFont()
+            .withEffect(EffectType.WAVE_UNDERSCORE, null)
+            .build());
+
+        builder.add(BashSyntaxHighlighter.SHEBANG_COMMENT, AttributesFlyweightBuilder.create()
+            .withBoldFont()
+            .build());
     }
 
     @Nonnull
     @Override
-    public String getColorSchemeFile() {
-        return "/colors/BashDefault.xml";
+    public String getColorSchemeId() {
+        return EditorColorsScheme.DEFAULT_SCHEME_NAME;
     }
 }
